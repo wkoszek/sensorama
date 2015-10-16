58e4dfb0fdcb39ad89870a59cc0b92f9de2e2689
Wojciech A. Koszek <wojciech@koszek.com>
2015-10-15 21:16:24 -0700

    Commit the perm. API file first.

---
+++ app/android/Sensorama/app/src/main/java/com/barvoy/sensorama/SRAPIPerms.java
@@ -0,0 +1,7 @@
+package com.barvoy.sensorama;
+
+
+public class SRAPIPerms {
+    public static final String parseAPIID = "PARSE_API_ID";
+    public static final String parseCLIID = "PARSE_CLIENT_ID";
+}
