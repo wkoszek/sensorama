dd2a6646a9c34d159b33978c0bfa7890e8d40a28
Wojciech A. Koszek <wojciech@koszek.com>
2015-10-17 01:57:45 -0700

    Regenerated and fixed the keys.

--- app/android/Sensorama/app/src/main/java/com/barvoy/sensorama/SRAPIPerms.java
+++ app/android/Sensorama/app/src/main/java/com/barvoy/sensorama/SRAPIPerms.java
@@ -2,6 +2,6 @@ package com.barvoy.sensorama;
 
 
 public class SRAPIPerms {
-    public static final String parseAPIID = "4pNPuPLledDT2C1tFKCsCNWW9fe9nmuI7Fi9ETRo";
-    public static final String parseCLIID = "vDh61bg5GKxHg5Rib4gcUTlY6o5mzfymku3HuXwa";
+    public static final String parseAPIID = "PARSE_API_ID";
+    public static final String parseCLIID = "PARSE_CLIENT_ID";
 }
