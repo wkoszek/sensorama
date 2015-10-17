68893658d9b27cb6d71e1ab46048636580a54156
Wojciech A. Koszek <wojciech@koszek.com>
2015-10-16 23:53:49 -0700

    Android Studio junk. I have no idea.

--- app/android/Sensorama/app/src/main/java/com/barvoy/sensorama/SRCfg.java
+++ app/android/Sensorama/app/src/main/java/com/barvoy/sensorama/SRCfg.java
@@ -8,4 +8,6 @@ public class SRCfg {
     public static final int buttonColorStopped = 0xffff0000;    // red
     public static final int buttonColorStart = 0xff00ff00;      // green
     public static final int buttonColorInitial = buttonColorStart;
+    public static final boolean doTestCrashReporting = false;
+    public static final boolean doParseBootstrap = false;
 }
